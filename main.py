6d2a39dda5aeb4e9b2027a33a13a388f5119201f
swyannnn <seowwengyann@gmail.com>
2025-11-25 21:56:20 +0800

    rename run name

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
     args.num_iterations = args.total_timesteps // args.batch_size
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    run_name = f"{args.env_id}__{args.algorithm}__{args.seed}__{int(time.time())}"
     if args.track:
         import wandb
 
